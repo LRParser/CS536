959f4dc422c890a8ffce4345d5c0c3a0b008aba7
Joe <jth82@drexel.edu>
2018-10-28 15:55:20 +0100

    Chaining multiple transformation matrices!

--- jth82_hw5/CG_hw5.cpp
+++ jth82_hw5/CG_hw5.cpp
@@ -249,11 +249,11 @@ Node* constructQuadNode(vector<point> vertices) {
     return baseSeparator;
 }
 
-point rotateVector(vector<vector<double>> matrix, point rhs) {
+point vectorMatMult(vector<vector<double>> matrix, point rhs) {
     point retval;
-    retval.x = (matrix[0][0] * rhs.x) + (matrix[0][1] * rhs.y) + (matrix[0][2] * rhs.z);
-    retval.y = (matrix[1][0] * rhs.x) + (matrix[1][1] * rhs.y) + (matrix[1][2] * rhs.z);
-    retval.z = (matrix[2][0] * rhs.x) + (matrix[2][1] * rhs.y) + (matrix[2][2] * rhs.z);
+    retval.x = (matrix[0][0] * rhs.x) + (matrix[0][1] * rhs.y) + (matrix[0][2] * rhs.z) + (matrix[0][3] * 1);
+    retval.y = (matrix[1][0] * rhs.x) + (matrix[1][1] * rhs.y) + (matrix[1][2] * rhs.z) + (matrix[1][3] * 1);
+    retval.z = (matrix[2][0] * rhs.x) + (matrix[2][1] * rhs.y) + (matrix[2][2] * rhs.z) + (matrix[2][3] * 1);
     return retval;
 }
 
@@ -266,6 +266,16 @@ vector<point> translateZ(vector<point> inputPoints, double zOffset) {
     for(auto it = inputPoints.begin(); it < inputPoints.end(); it++) {
         point p = *it;
         p.z += zOffset;
+        returnPoints.push_back(p);
+    }
+    return returnPoints;
+}
+
+vector<point> translateY(vector<point> inputPoints, double yOffset) {
+    vector<point> returnPoints;
+    for(auto it = inputPoints.begin(); it < inputPoints.end(); it++) {
+        point p = *it;
+        p.y += yOffset;
         returnPoints.push_back(p);
     }
     return returnPoints;
@@ -295,6 +305,104 @@ vector<point> drawQuad(point ll, point ur) {
 
     return returnVec;
 
+}
+
+vector<vector<double>> getIdentityMatrix() {
+    vector<vector<double>> result;
+
+    for(int i = 0; i < 4; i++) {
+        vector<double> row;
+        for(int j = 0; j < 4; j++) {
+            row.push_back(0.0);
+        }
+        result.push_back(row);
+    }
+
+    // Now make it an identity matrix
+    result[0][0] = 1.0;
+    result[1][1] = 1.0;
+    result[2][2] = 1.0;
+    result[3][3] = 1.0;
+
+    return result;
+}
+
+vector<vector<double>> getYRotationMatrix(double thetaRadians) {
+
+    vector<vector<double>> rotationMatrix = getIdentityMatrix();
+
+    rotationMatrix[0][0] = cos(thetaRadians);
+    rotationMatrix[0][2] = sin(thetaRadians);
+
+    rotationMatrix[2][0] = -1*sin(thetaRadians);
+    rotationMatrix[2][2] = cos(thetaRadians);
+
+    return rotationMatrix;
+}
+
+double dot(vector<double> row, vector<double> column) {
+    double retVal = 0.0;
+    for(int i = 0; i < 4; i++) {
+        retVal += row[i]*column[i];
+    }
+    return retVal;
+}
+
+vector<vector<double>> matMult(vector<vector<double>> lhs, vector<vector<double>> rhs) {
+    vector<vector<double>> result;
+
+    for(int i = 0; i < 4; i++) {
+        vector<double> row;
+        for(int j = 0; j < 4; j++) {
+            row.push_back(0.0);
+        }
+        result.push_back(row);
+    }
+
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            result[i][j] = dot(lhs[i],rhs[j]);
+            // Find dot product of i row of lhs and j column of rhs
+        }
+    }
+    return result;
+}
+
+
+
+vector<vector<double>> getTranslationMatrix(point p) {
+        vector<vector<double>> result;
+
+        for(int i = 0; i < 4; i++) {
+            vector<double> row;
+            for(int j = 0; j < 4; j++) {
+                row.push_back(0.0);
+            }
+            result.push_back(row);
+        }
+
+        // Now make it an identity matrix
+        result[0][0] = 1.0;
+        result[1][1] = 1.0;
+        result[2][2] = 1.0;
+        result[3][3] = 1.0;
+
+
+    // Plus add in the desired points
+        result[0][3] = p.x;
+        result[1][3] = p.y;
+        result[2][3] = p.z;
+
+        return result;
+}
+
+vector<point> applyTransformationMatrixToPoints(vector<point> input, vector<vector<double>> matrix) {
+    vector<point> retVal;
+    for(auto it = input.begin(); it < input.end(); it++) {
+        point p = *it;
+        retVal.push_back(vectorMatMult(matrix, p));
+    }
+    return retVal;
 }
 
 int
@@ -365,6 +473,7 @@ main(int argc, char ** argv) {
     Node *root = new Node("", "", "");
 
     float zTranslation = 0.0;
+    double baseHeight = 1.0;
 
     // Draw base
     point baseLL(-2,-2,0);
@@ -372,157 +481,70 @@ main(int argc, char ** argv) {
     vector<point> baseVertices = drawQuad(baseLL, baseUR);
     Node* baseSeparator = constructQuadNode(baseVertices);
     root->addChild(baseSeparator);
-    zTranslation += 1;
 
     // Draw link 1
     point l1LL(-.5,-.5,0);
     point l1UR(.5,.5,link1Length);
     vector<point> link1Vertices = drawQuad(l1LL, l1UR);
-    link1Vertices = translateZ(link1Vertices, zTranslation);
+    point link1Translation;
+    link1Translation.z = baseHeight;
+    vector<vector<double>> translationMatrixLink1 = getTranslationMatrix(link1Translation);
+    vector<vector<double>> rotationMatrixLink1 = getIdentityMatrix(); // Update later
+
+    vector<vector<double>> transformationMatrixLink1 = translationMatrixLink1; //  matMult(translationMatrixLink1,rotationMatrixLink1);
+    link1Vertices = applyTransformationMatrixToPoints(link1Vertices, transformationMatrixLink1);
     Node* link1Separator = constructQuadNode(link1Vertices);
     root->addChild(link1Separator);
     zTranslation += link1Length;
 
     // Draw link 2
-    float l2 = 3.0;
-    theta2 = 37.0;
+
     double theta2Radians = to_radians(theta2);
 
     // Now to apply a rotation, create rotation matrix
-    vector<double> row1;
-    row1.push_back(cos(theta2Radians));
-    row1.push_back(0);
-    row1.push_back(sin(theta2Radians));
+    point translationValsLink2;
+    translationValsLink2.z = link1Length;
+    vector<vector<double>> translationMatrixLink2 = getTranslationMatrix(translationValsLink2);
+    vector<vector<double>> rotationMatrixLink2 = getYRotationMatrix(theta2Radians);
 
-    vector<double> row2;
-    row2.push_back(0);
-    row2.push_back(1);
-    row2.push_back(0);
-
-    vector<double> row3;
-    row3.push_back(-1 * sin(theta2Radians));
-    row3.push_back(0);
-    row3.push_back(cos(theta2Radians));
-
-    vector<vector<double>> rotationMatrixLink2;
-    rotationMatrixLink2.push_back(row1);
-    rotationMatrixLink2.push_back(row2);
-    rotationMatrixLink2.push_back(row3);
+    vector<vector<double>> link2TransformationMatrix = matMult(transformationMatrixLink1,matMult(rotationMatrixLink2,translationMatrixLink2));
 
     point l2LL(-.5,-.5,0);
-    point l2UR(.5,.5,l2);
+    point l2UR(.5,.5,link2Length);
 
     vector<point> link2Vertices = drawQuad(l2LL, l2UR);
 
-    vector<point> link2VerticesRotated;
-    for(auto it = link2Vertices.begin(); it < link2Vertices.end(); it++) {
-        point p = *it;
-        link2VerticesRotated.push_back(rotateVector(rotationMatrixLink2, p));
-    }
+    vector<point> link2VerticesRotatedTranslated = applyTransformationMatrixToPoints(link2Vertices, link2TransformationMatrix);
 
-    vector<point> link2VerticesRotatedTranslated = translateZ(link2VerticesRotated, zTranslation);
-    
     Node* link2Separator = constructQuadNode(link2VerticesRotatedTranslated);
     root->addChild(link2Separator);
-    zTranslation += l2;
-    /*
+    zTranslation += link2Length;
 
-    // Old method for base
+    // Draw link 3
 
-    point p0(2.000000,2.000000,1.000000); // upper right
-    point p1(-2.000000, 2.000000, 1.000000);
-    point p2(-2.000000, -2.000000, 1.000000);
-    point p3(2.000000, -2.000000, 1.000000);
-    point p4(2.000000, 2.000000, 0.000000);
-    point p5(-2.000000, 2.000000, 0.000000);
-    point p6(-2.000000, -2.000000, 0.000000); // lower left
-    point p7(2.000000, -2.000000, 0.000000);
+    cerr << theta3 << endl;
+    cerr << link3Length << endl;
 
-    vector<point> baseVertices;
+    double theta3Radians = to_radians(theta2 + theta3);
+    point translationValsLink3;
+    translationValsLink2.z = link2Length;
+    vector<vector<double>> translationMatrixLink3 = getTranslationMatrix(translationValsLink2);
+    vector<vector<double>> rotationMatrixLink3 = getYRotationMatrix(theta3Radians);
 
-    baseVertices.push_back(p0);
-    baseVertices.push_back(p1);
-    baseVertices.push_back(p2);
-    baseVertices.push_back(p3);
-    baseVertices.push_back(p4);
-    baseVertices.push_back(p5);
-    baseVertices.push_back(p6);
-    baseVertices.push_back(p7);
+    vector<vector<double>> link3TransformationMatrix = matMult(link2TransformationMatrix,matMult(rotationMatrixLink3,translationMatrixLink3));
 
-    // Nodes for link 1
+    point l3LL(-.5,-.5,0);
+    point l3UR(.5,.5,link3Length);
 
-    vector<point> link1Vertices;
+    vector<point> link3Vertices = drawQuad(l3LL, l3UR);
 
-    point p8(0.703233, -0.073913, 5.000000);
-    point p9(0.073913, 0.703233, 5.000000);
-    point p10(-0.703233, 0.073913, 5.000000);
-    point p11(-0.073913, -0.703233, 5.000000);
-    point p12(0.703233, -0.073913, 1.000000);
-    point p13(0.073913, 0.703233, 1.000000);
-    point p14(-0.703233, 0.073913, 1.000000);
-    point p15(-0.073913, -0.703233, 1.000000);
+    vector<point> link3VerticesRotatedTranslated = applyTransformationMatrixToPoints(link3Vertices, link3TransformationMatrix);
 
-    link1Vertices.push_back(p8);
-    link1Vertices.push_back(p9);
-    link1Vertices.push_back(p10);
-    link1Vertices.push_back(p11);
-    link1Vertices.push_back(p12);
-    link1Vertices.push_back(p13);
-    link1Vertices.push_back(p14);
-    link1Vertices.push_back(p15);
 
-    Node* link1Separator = constructQuadNode(link1Vertices);
-    root->addChild(link1Separator);
-
-    // Nodes for link 2
-
-    vector<point> link2Vertices;
-
-    point p16(1.821242, -1.454539, 7.016778);
-    point p17(1.332169, -0.850583, 7.646098);
-    point p18(0.555023, -1.479904, 7.646098);
-    point p19(1.044096, -2.083860, 7.016778);
-    point p20(0.633110, 0.012682, 4.685340);
-    point p21(0.144036, 0.616638, 5.314660);
-    point p22(-0.633110, -0.012682, 5.314660);
-    point p23(-0.144036, -0.616638, 4.685340);
-
-    link2Vertices.push_back(p16);
-    link2Vertices.push_back(p17);
-    link2Vertices.push_back(p18);
-    link2Vertices.push_back(p19);
-    link2Vertices.push_back(p20);
-    link2Vertices.push_back(p21);
-    link2Vertices.push_back(p22);
-    link2Vertices.push_back(p23);
-
-    Node* link2Separator = constructQuadNode(link2Vertices);
-    root->addChild(link2Separator);
-
-    // Nodes for link 3
-    vector<point> link3Vertices;
-
-    point p24(3.027149, -2.943710, 6.241485);
-    point p25(3.179396, -3.131719, 7.211781);
-    point p26(2.402250, -3.761039, 7.211781);
-    point p27(2.250004, -3.573031, 6.241485);
-    point p28(1.500582, -1.058557, 6.846290);
-    point p29(1.652829, -1.246566, 7.816586);
-    point p30(0.875683, -1.875886, 7.816586);
-    point p31(0.723436, -1.687877, 6.846290);
-
-    link3Vertices.push_back(p24);
-    link3Vertices.push_back(p25);
-    link3Vertices.push_back(p26);
-    link3Vertices.push_back(p27);
-    link3Vertices.push_back(p28);
-    link3Vertices.push_back(p29);
-    link3Vertices.push_back(p30);
-    link3Vertices.push_back(p31);
-
-    Node* link3Separator = constructQuadNode(link3Vertices);
+    Node* link3Separator = constructQuadNode(link3VerticesRotatedTranslated);
     root->addChild(link3Separator);
-    */
+    zTranslation += link3Length;
+
 
     if(debug) {
         cerr << "Serializing to OI format" << endl;
